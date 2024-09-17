9f0189be0023068104bb9e00ed866d0d466b970e
hrldspnz <haroldespinoza823@gmail.com>
2024-09-16 18:10:34 -0600

    Compilador base

---
+++ Compilador/codes.py
@@ -0,0 +1,50 @@
+# Archivo con los codigos para cada instruccion y registro
+
+# Codigo para los registros
+regs = {
+    'R0':'0000',
+    'R1':'0001',
+    'R2':'0010',
+    'R3':'0011',
+    'R4':'0100',
+    'R5':'0101',
+    'R6':'0110',
+    'R7':'0111',
+    'R8':'1000',
+    'R9':'1001',
+    'R10':'1010',
+    'R11':'1011',
+    'R12':'1100',
+    'R13':'1101',
+    'R14':'1110',
+    'R15':'1111'
+
+}
+
+# Codigos para las instrucciones
+inst = {
+    'CHOCO':'0000', #NOP
+    'SUB':'0001',
+    'MUL':'0010',
+    'DIV':'0011',
+    'AND':'0100',
+    'OR':'0101',
+    'SRL':'0110',
+    'SLL':'0111',
+    'ADD':'1000',
+    'LDI':'1001',
+    'STR':'1010',
+    'MOV':'1011',
+    'BETO':'1100',
+    'JASON': '1101',
+    'BNE':'1110',
+    'RET': '1111'
+}
+
+Fillers = {
+    'INS_FILL':4,
+    'DOUBLE_FILL':8,
+    '':'',
+    '':'',
+    '':'',
+}
