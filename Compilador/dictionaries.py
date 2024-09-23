3365aa682411f74a92ab50c6d5aeaf2a7c26d633
hrldspnz <haroldespinoza823@gmail.com>
2024-09-23 17:49:08 -0600

    dictionaries update

--- Compilador/dictionaries.py
+++ Compilador/dictionaries.py
@@ -15,28 +15,39 @@ END = 'end'
 
 #scalars
 
-ADD = 'add'             #suma
-ADDIMM = 'addi'         #suma inmediato
-MULT = 'mult'           #multiplicacion
-MULTIMM = 'multi'       #multiplicacion inmediato
-EDIV = 'dive'           #division entera
-MOD = 'mod'             #modulo
-LOAD = 'loadw'          #load
-LDIMM = 'ldi'           #load inmediato
-STORE = 'storew'        #store
-CMP = 'comp'            #comparar
-NOP = 'choco'           #stall
-BETO = 'beto'           #branch equal to
-JMP = 'jump'            #salto
+ADD = 'add'             #suma (R)
+ADDIMM = 'addi'         #suma inmediato (I)
+SUB = 'sub'             #resta (R)
+
+#sin estructura
+SHIFTLL = 'sll'         #shift left logical (R)
+SHIFTRL = 'srl'         #shift right logical (R)
+XOR = 'xor'             #xor logico (R)
+AND = 'and'             #and logico (R)
+MULT = 'mult'           #multiplicacion (R)
+
+BETO = 'beq'            #branch equal (I)
+BLT = 'blt'             #branch less than (I)
+STW = 'sw'              #store word (I)
+LDW = 'lw'              #load word (I)
+JMP = 'j'               #salto (J)
 
 #vectorial
-VADD = 'vecadd'         #suma vectorial
-VMUL = 'vecmult'        #multiplicacion vectorial
-VXOR = 'vecxor'         #xor vectorial
-BOX_REPLACE = 'box_r'   #reemplaza un un elemento del vector por su equivalente en el SBOX
-VLOAD = 'vecld'         #load vectorial
-VLDIMM = 'vecldi'       #load inmediato vectorial
-VSTR = 'vecstr'         #store vectorial
+
+VSET = 'vset'           #set vectorial (I)
+VADD = 'vadd'           #suma vectorial (R)
+VMUL = 'vmult'          #multiplicacion vectorial (R)
+VSUM = 'vsum'           #sumatoria vectorial (R)
+
+#sin estructura
+VSHIFTLL = 'vsll'       #shift left logical vectorial (R)
+VSHIFTRL = 'vsrl'       #shift right logical vectorial (R)
+VXOR = 'vxor'           #xor logico vectorial (R)
+VAND = 'vand'           #and logico vectorial (R)
+
+VST = 'vst'             #store vectorial (I)
+VLD = 'vld'             #load vectorial (I)
+
 
 isa = {
     #con listo me refiero a que estan referenciados en interpreter.py 
@@ -46,48 +57,60 @@ isa = {
     END: {'type': None, 'opcode': '110001'},
     
     # Escalares
-    ADD: {'type': R, 'opcode': '000000', 'funct': '100000'},  #listo
-    ADDIMM: {'type': I, 'opcode': '001000'}, #listo
-    MULT: {'type': R, 'opcode': '000000', 'funct': '011000'}, #listo
-    MULTIMM: {'type': I, 'opcode': '001001'}, #listo
-    EDIV: {'type': R, 'opcode': '000000', 'funct': '011010'}, #listo
-    MOD: {'type': R, 'opcode': '001010'}, #listo
-    LOAD: {'type': I , 'opcode': '100011'},
-    LDIMM: {'type': I, 'opcode': '001011'}, #listo
-    STORE: {'type': I, 'opcode': '101011'},
-    CMP: {'type': I, 'opcode': '000111'},
-    NOP: {'type': None, 'opcode': '000000'},  # tomado como no-operation
-    BETO: {'type': I, 'opcode': '000100'}, #listo
-    JMP: {'type': J, 'opcode': '000010'}, #listo
+
+    ADD: {'type': R, 'opcode': '000000', 'funct': '000000'},  #traducido
+    SUB : {'type': R, 'opcode': '000000', 'funct': '000001'}, #trad
+    ADDIMM: {'type': I, 'opcode': '010000'},  #trad
+     
+
+    #sin estructura
+    SHIFTLL : {'type': R, 'opcode': '000000', 'funct': '100000'},  
+    SHIFTRL: {'type': R, 'opcode': '000000', 'funct': '100000'},  
+    XOR: {'type': R, 'opcode': '000000', 'funct': '100000'},  
+    AND: {'type': R, 'opcode': '000000', 'funct': '100000'},  
+    MULT: {'type': R, 'opcode': '000000', 'funct': '100000'},  
+
+    BETO: {'type': I, 'opcode': '100000'}, #trad 
+    BLT: {'type': I, 'opcode': '100001'}, #trad
+    STW: {'type': I, 'opcode': '010001'}, #trad
+    LDW: {'type': I, 'opcode': '010010'}, #trad
+    JMP: {'type': J, 'opcode': '100010'}, #trad
 
     # Vectoriales
-    VADD: {'type': R, 'opcode': '010100', 'funct': '100000'}, #listo
-    VMUL: {'type': R, 'opcode': '010100', 'funct': '000010'}, #listo
-    VXOR: {'type': R, 'opcode': '010100', 'funct': '001010'}, #listo
-    BOX_REPLACE: {'type': R, 'opcode': '010101', 'funct': '001011'}, #listo pero no implementado
-    VLOAD: {'type': I, 'opcode': '101110'},
-    VLDIMM: {'type': I, 'opcode': '001100'}, #listo
-    VSTR: {'type': I, 'opcode': '101111'},
+
+    VSET: {'type': I, 'opcode': '010100', 'funct': '100000'},
+    VADD: {'type': R, 'opcode': '001100', 'funct': '000000'}, #trad
+    VMUL: {'type': R, 'opcode': '001100', 'funct': '000010'}, #trad
+    VSUM: {'type': R, 'opcode': '010100', 'funct': '100000'}, 
+
+    #sin estructura
+    VSHIFTLL: {'type': R, 'opcode': '010100', 'funct': '100000'},
+    VSHIFTRL: {'type': R, 'opcode': '010100', 'funct': '100000'},
+    VXOR: {'type': R, 'opcode': '010100', 'funct': '100000'},
+    VAND: {'type': R, 'opcode': '010100', 'funct': '100000'},
+
+    VST: {'type': I, 'opcode': '011101'}, #trad
+    VLD: {'type': I, 'opcode': '011110'}, #trad
+
 }
 
 #Registers names
 registers = {
     '$zero': '00000',
-    '$a0': '00001', 
-    '$a1': '00010', 
-    '$sp': '00011', 
-    '$cbh': '00100', 
-    '$cbt': '00101', 
-    '$b0': '00110', 
-    '$b1': '00111', 
-    '$b2': '01000', 
-    '$b3': '01001', 
-    '$b4': '01010', 
-    '$b5': '01011', 
-    '$b6': '01100', 
-    '$b7': '01101', 
-    '$b8': '01110', 
-    '$b9': '01111',
+    '$t0': '00001', 
+    '$t1': '00010', 
+    '$t2': '00011', 
+    '$t3': '00100', 
+    '$t4': '00101', 
+    '$t5': '00110', 
+    '$t6': '00111', 
+    '$t8': '01000', 
+    '$t9': '01001', 
+    '$t10': '01010', 
+    '$t11': '01011', 
+    '$t12': '01100', 
+    '$t13': '01101', 
+    '$t14': '01110', 
     '$v0': '10000', 
     '$v1': '10001', 
     '$v2': '10010', 
@@ -96,10 +119,10 @@ registers = {
     '$v5': '10101', 
     '$v6': '10110', 
     '$v7': '10111', 
-    '$': '01000', 
-    '$': '01001', 
-    '$': '01010', 
-    '$': '01011', 
+    '$stall': '01000', 
+    '$cpi': '01001', 
+    '$ac': '01010', 
+    '$mem': '01011', 
     '$': '01100', 
     '$': '01101', 
     '$': '01110', 
