bbf8dac4cc0617c9e8c95bf3d024bf1db4855da1
hrldspnz <haroldespinoza823@gmail.com>
2024-09-25 22:34:55 -0600

    examples

---
+++ Compilador/example.asm
@@ -0,0 +1,4 @@
+addi $t0,$zero,0xF
+addi $t1,$zero, 0x10
+add $t0, $t0, $t1
+addi $t1, $t1 0x100
--- Compilador/interpreter_test.asm
+++
@@ -1,6 +0,0 @@
-addi $t0,$zero,0x8888
-addi $t1,$zero, 0x100
-sw $t1, $t0 ,0
-addi $t0, $t0,0x0ABA
-addi $t1, $t1 0x100
-sw $t1, $t0,4
